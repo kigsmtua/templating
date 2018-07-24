4a8da32dcbc49fb49632cd5fb8b9d744360e9f37
John Kiragu <mutuakiragu@gmail.com>
2018-07-24 13:54:48 +0300

    Expression 1

--- src/main/java/com/kiragu/templating/Main.java
+++ src/main/java/com/kiragu/templating/Main.java
@@ -9,6 +9,8 @@ import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import com.kiragu.models.Customer;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 /**
  *
@@ -21,11 +23,23 @@ public class Main {
      Expression exp = parser.parseExpression("'Hello World'.bytes.length");
      int length = (Integer) exp.getValue();
 //   The expressions can be used here
+     Customer customer = new Customer(2,"John","2543","1322",4);
+     expression1(customer);
      System.out.println("The length returned is..." + length);
     }
     
-    public static String expression1(Customer customer){
-        return "name";
+    public static void expression1(Customer customer){
+     
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("riskBand==4");
+        EvaluationContext context = new StandardEvaluationContext(customer);
+        boolean result = exp.getValue(context, Boolean.class);
+        
+        if (result){
+            System.out.println("Journey A");
+        }else{
+            System.out.println("Journey B");
+        }
     }
     
     public static String expression2(Customer customer){
