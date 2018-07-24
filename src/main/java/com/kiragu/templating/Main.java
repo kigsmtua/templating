6b2c17152abb03b21d5d670c2eb01123a776f858
vintuwei <vintuwei@gmail.com>
2018-07-24 16:20:21 +0300

    Added loops

--- src/main/java/com/kiragu/templating/Main.java
+++ src/main/java/com/kiragu/templating/Main.java
@@ -25,27 +25,55 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 public class Main {
     
     public static void main(String[] args) {
-  
-     Customer customer = new Customer(2,"John","2543","1322",4);
+    	
+    Customer customer = new Customer(2,"John","2543","1322",4);
+    double total = 0;
+    for (int i = 0; i < 1000; i++) {
+    	long startTime = System.nanoTime();
+    	customer = new Customer(2 + i,"John","2543","1322",4 + i);
+    	expression1(customer);
+    	long endTime = System.nanoTime();
+    	total +=  (endTime - startTime) / 1000000;
+    }
+    
+    System.out.println("Express 1 takes an average of " + (total / 1000));
+    
+    
+    customer = new Customer(3,"Vincent","432432","44444",3);
+    Calendar c=new GregorianCalendar();
+    c.add(Calendar.DATE, 31);
+    Date futureDate=c.getTime();
+    Loan loan = new Loan(1, customer, 999, futureDate, 14.1);
+    
+    total = 0;
+    for (int i = 0; i < 1; i++) {
+    	long startTime = System.nanoTime();
+        // Case 1 for Expression 2
+        expression2(loan);
+    	long endTime = System.nanoTime();
+    	total +=  Long.valueOf((endTime - startTime)) / 1000000;
+    }
+    
+    
+    System.out.println("Express 2 takes an average of " + (total / 1000));
      
-     expression1(customer);
-     
-     
-     customer = new Customer(3,"Vincent","432432","44444",3);
-     
-     Calendar c=new GregorianCalendar();
-     c.add(Calendar.DATE, 31);
-     Date futureDate=c.getTime();
-     
-     // Case 1 for Expression 2
-     Loan loan = new Loan(1, customer, 999, futureDate, 14.1);
-     expression2(loan);
-     
-     // Case 2 for Expression 3
-     loan = new Loan(1, customer, 2000, new Date(), 3);
-     customer = new Customer(4,"John","254722123456","44444",3);
-     loan.setCustomer(customer);
-     expression2(loan);
+	 // Case 2 for Expression 3
+	 loan = new Loan(1, customer, 2000, new Date(), 3);
+	 customer = new Customer(4,"John","254722123456","44444",3);
+	 loan.setCustomer(customer);
+	 
+	 
+	 total = 0;
+	    for (int i = 0; i < 1; i++) {
+	    	long startTime = System.nanoTime();
+	        // Case 1 for Expression 2
+	    	expression2(loan);
+	    	long endTime = System.nanoTime();
+	    	total +=  Long.valueOf((endTime - startTime)) / 1000000;
+	    }
+	    
+	    
+	    System.out.println("Express 2 Case 2 takes an average of " + (total / 1000));
     }
     
     public static void expression1(Customer customer){
@@ -56,13 +84,13 @@ public class Main {
         boolean result = exp.getValue(context, Boolean.class);
         
         if (result){
-            System.out.println("expression1 Journey A");
+//            System.out.println("expression1 Journey A");
         }else{
-            System.out.println("expression1 Journey B");
+//            System.out.println("expression1 Journey B");
         }  
         long endTime = System.nanoTime();
         long duration = (endTime - startTime)/1000000;
-        System.out.println("Expresssion one executed in..." +duration +"ms");
+//        System.out.println("Expresssion one executed in..." +duration +"ms");
     }
     
     public static void expression2(Loan loan){
@@ -86,21 +114,21 @@ public class Main {
 
         boolean result = exp1.getValue(context, Boolean.class);
         
-        String params = "Amount " + loan.getAmount() + " Due Date: " + loan.getDueDate() + 
-        		" Interest: " + loan.getInterest() + " Name: " + loan.getCustomer().getName() +
-        		" MSISDN " + loan.getCustomer().getMsisdn();
+//        String params = "Amount " + loan.getAmount() + " Due Date: " + loan.getDueDate() + 
+//        		" Interest: " + loan.getInterest() + " Name: " + loan.getCustomer().getName() +
+//        		" MSISDN " + loan.getCustomer().getMsisdn();
         
         if (result){
-            System.out.println("expression2 Journey B - " + params);
+//            System.out.println("expression2 Journey B - " + params);
         }else if (exp2.getValue(context, Boolean.class)){
         	
-            System.out.println("expression2 Journey A - " + params);
+//            System.out.println("expression2 Journey A - " + params);
         } else {
-        	System.out.println("expression2 Journey B - " + params);
+//        	System.out.println("expression2 Journey B - " + params);
         }
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime)/1000000;
-        System.out.println("Expresssion two executed in... " +duration +" ms");
+//        long endTime = System.nanoTime();
+//        long duration = (endTime - startTime)/1000000;
+//        System.out.println("Expresssion two executed in... " +duration +" ms");
     }
      
     
